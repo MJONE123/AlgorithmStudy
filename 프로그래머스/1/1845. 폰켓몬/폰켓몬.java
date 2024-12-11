3badc9d7d221251bf836d382dfbac8f5dd6b8e1a
문준일 <130478251+MJONE123@users.noreply.github.com>
2024-12-11 16:33:01 +0900

    [level 1] Title: 폰켓몬, Time: 6.29 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int num = nums.length/2;
+        Set<Integer> set = new HashSet<>();
+
+        for(int n : nums) {
+            set.add(n);
+        }
+
+        if(set.size()<num) {
+            return set.size();
+        }
+        return num;
+    }
+}
